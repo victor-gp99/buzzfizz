17269fb61780fe1c09775d0eda0d618fb956585c
victor-gp99 <victoreduardogpat99@outlook.com>
2022-05-10 18:52:41 -0500

    Realizando refactoring... agregando services/fizzbuzzservice.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Reader = require("./lib/Utils/Reader")
 const ExplorerService = require ("./lib/Services/ExplorerService")
+const FizzbuzzService = require("./lib/Services/FizzbuzzService")
 
 const explorers = Reader.readJsonFile("explorers.json")
 //console.log(explorers)
@@ -8,3 +9,17 @@ const explorers = Reader.readJsonFile("explorers.json")
 console.log(ExplorerService.filterByMission(explorers, "node"))
 console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"))
 console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"))
+
+const explorer1 = {name: "Explorer1", score: 1}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer1))// {name: "Explorer1", score: 1, trick: 1} 
+
+const explorer3 = {name: "Explorer3", score: 3}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer3))// {name: "Explorer3", score: 3, trick: "FIZZ"}
+
+const explorer5 = {name: "Explorer5", score: 5}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer5))// {name: "Explorer5", score: 5, trick: "BUZZ"}
+
+const explorer15 = {name: "Explorer15", score: 15}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer15))// {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
+
+
