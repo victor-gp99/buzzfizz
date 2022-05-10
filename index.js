806aefc42d4fa52d438b05e10b0179e6eded2dc2
victor-gp99 <victoreduardogpat99@outlook.com>
2022-05-10 18:20:45 -0500

    Realizando refactoring... agregando services/explorerservice.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const Reader = require("./lib/Utils/Reader")
+const ExplorerService = require ("./lib/Services/ExplorerService")
 
 const explorers = Reader.readJsonFile("explorers.json")
-console.log(explorers)
+//console.log(explorers)
+
+// Aplicación del ExplorerService sobre la lista de explorers
+console.log(ExplorerService.filterByMission(explorers, "node"))
+console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"))
+console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"))
